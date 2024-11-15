c5f296ab5e65241a9a74a88a5010a34c07346e5f
Alecto <fomenkoandrey1978@gmail.com>
2024-11-15 19:13:25 +0200

    upd

--- index.js
+++
@@ -1,9 +0,0 @@
-import SwipeCarousel from './swipe-carousel.js'
-
-const carousel = new SwipeCarousel({
-  containerId: '#mycarousel',
-  // slideID: '.slide',
-  interval: 1000,
-  // isPlaying: false,
-})
-carousel.init()
